e2e60e2ccb06b5044db5b780b90bc927fca84cf1
baggiowangyu <baggiowangyu@gmail.com>
2017-09-10 23:26:23 +0800

    1.增加ffmpeg开发包 2.增加winpcap开发包

---
+++ src/MediaDemo/MediaDemo.cpp
@@ -0,0 +1,32 @@
+// MediaDemo.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "libavformat/avformat.h"
+#ifdef __cplusplus
+};
+#endif
+
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout<<"========FFmepg学习教程========"<<std::endl;
+	std::cout<<"1. 枚举当前"<<std::endl;
+	std::cout<<""<<std::endl;
+	std::cout<<""<<std::endl;
+	std::cout<<""<<std::endl;
+	std::cout<<""<<std::endl;
+	std::cout<<""<<std::endl;
+	std::cout<<""<<std::endl;
+	std::cout<<""<<std::endl;
+	std::cout<<""<<std::endl;
+
+	return 0;
+}
+
